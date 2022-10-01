7bd21074fabc56af92b67bee0f622fd1f3fc9b2d
Pavel <mister.castle@yandex.ru>
2022-10-01 12:19:22 +0300

    сем 4 задание 25

---
+++ task4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+int constA = number("");
+int constB = number("");
+stepen(constA, constB);
+void stepen(int a, int b)
+{
+    int x= 1;
+    for (int i = 1
+    ; i<=b; i++)
+    {
+        x = x * a;
+    }
+    Console.WriteLine(x);
+}
+
+
+int number(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
