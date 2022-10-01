691084f3a5074062d5dfd08346b250664fb63cf6
Pavel <mister.castle@yandex.ru>
2022-10-01 12:59:06 +0300

    сем 4 задание 27

---
+++ task5/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+int a = number("");
+int lenght = Len(a);
+sum(a, lenght);
+int Len(int x)
+{
+    int i = 0;
+    while (x>0)
+    {x= x/10; i++;}
+    return i;
+}
+void sum(int n, int lenght)
+{
+    int y = 0;
+    for (int k = 1; k <=lenght; k++)
+    {y += n %10; n=n/10;}
+    Console.WriteLine(y);
+}
+int number(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
