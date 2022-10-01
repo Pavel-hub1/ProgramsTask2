a46ad82f9c59ec7b6a32fb30fd246937098ea665
Pavel <mister.castle@yandex.ru>
2022-10-01 13:09:50 +0300

    сем 4 задание 29

---
+++ task6/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+int x = number("длина массива  ");
+int[] random = new int[x];
+for (int i=0; i<random.Length; i++)
+{
+    random[i] = new Random().Next(1,9);
+    Console.Write(random[i] + "");
+}
+
+int number(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
