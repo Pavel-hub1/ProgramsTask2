7e5133e68d06804223d80c1d697cd8e656d86d15
Pavel <you@example.com>
2022-10-21 20:27:05 +0300

    задание 68 семинар 9

---
+++ task18/Program.cs
@@ -0,0 +1,24 @@
+﻿int valeus(string a)
+{
+    Console.Write(a);
+    int numbers = Convert.ToInt32(Console.ReadLine());
+    return numbers;
+}
+
+
+int M = valeus("M: ");
+int N = valeus("N: ");
+
+int function = func(M,N);
+
+int func(int M, int N)
+{
+    if (M==0)
+        return N+1;
+    else if (N==0)
+        return func(M-1, 1);
+        else return func(M-1, func(M,N-1));
+}
+
+Console.Write($"result:  {function}");
+
