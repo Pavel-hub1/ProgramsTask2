f97184201425fa522574145ea7a48a1a5be2acd1
Pavel <you@example.com>
2022-10-21 20:14:44 +0300

    задание 66 семинар 9

---
+++ task17/Program.cs
@@ -0,0 +1,24 @@
+﻿int valeus(string a)
+{
+    Console.Write(a);
+    int numbers = Convert.ToInt32(Console.ReadLine());
+    return numbers;
+}
+
+
+int M = valeus("M: ");
+int N = valeus("N: ");
+
+void sum (int M, int N, int count)
+{
+    if (M>N)
+    {
+        Console.WriteLine($"{count}");
+        return;
+    }
+    count = count + (M++);
+    sum(M, N, count);
+
+}
+
+sum(M,N, 0);
